44ca974a4d2523d09cb399b256b1ba23b16db730
Leon <xiaoyankeji@hotmail.com>
2017-08-23 15:25:33 +0800

    首次提交

---
+++ Speleon-Client/LoginForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Speleon_Client
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            this.Icon = UnityResource.Speleon;
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
